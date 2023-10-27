677a4f5886383253b2e45564340c79386cac4364
Saloni Gupta <70319586+saloni24gupta@users.noreply.github.com>
2023-10-27 23:32:59 +0530

    navstart

--- mongodb/src/LinkedIn/vite-project/src/App.jsx
+++ mongodb/src/LinkedIn/vite-project/src/App.jsx
@@ -1,19 +1,23 @@
 import Login from "./components/Login"
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./components/Home"
+import Header from "./components/Header"
 function App() {
-  
+
 
   return (
     <>
-   
-    <div className="App">
-      <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login/>} ></Route>
-      </Routes>
-      </BrowserRouter>
-      
-    </div>
+
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Login />} ></Route>
+            <Route path="/home" element={<Home />} ></Route>
+         
+          </Routes>
+        </BrowserRouter>
+
+      </div>
     </>
   )
 }
--- mongodb/src/LinkedIn/vite-project/src/components/Home.jsx
+++ mongodb/src/LinkedIn/vite-project/src/components/Home.jsx
@@ -1,1 +1,14 @@
- 
+import styled from "styled-components"
+import Header from "./Header";
+
+const Home = (props) => {
+    return (
+        <>
+        <div >Home</div>
+    
+      <Header />
+      </>
+    )
+}
+
+export default Home;
