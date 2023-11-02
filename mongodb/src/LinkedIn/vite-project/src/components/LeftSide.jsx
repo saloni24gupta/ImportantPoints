4c1fb578fbd0e185e21bb18f02c1216050493512
Saloni Gupta <70319586+saloni24gupta@users.noreply.github.com>
2023-11-02 22:12:41 +0530

    leftsidecom

--- mongodb/src/LinkedIn/vite-project/src/components/LeftSide.jsx
+++ mongodb/src/LinkedIn/vite-project/src/components/LeftSide.jsx
@@ -1,21 +1,53 @@
 import styled from "styled-components"
 
 const LeftSide = (props) => {
-    return(
+    return (
         <Container>
-<ArtCard>
-    <UserInfo>
-        <CardBackGround />
-            <a>
-                <Photo />
-                <Link> Welcome, there</Link>
-            </a>
-          <a>
-            <AddPhotoText>Add a photo</AddPhotoText>
-          </a>
-       
-    </UserInfo>
-</ArtCard>
+            <ArtCard>
+                <UserInfo>
+                    <CardBackGround />
+                    <a>
+                        <Photo />
+                        <Link> Welcome, there</Link>
+                    </a>
+                    <a>
+                        <AddPhotoText>Add a photo</AddPhotoText>
+                    </a>
+
+                </UserInfo>
+
+                <Widget>
+                    <a>
+                        <div>
+                            <span>Connections</span>
+                            <span>Grow your network</span>
+                        </div>
+                        <img src="/assets/widget-icon.svg"></img>
+                    </a>
+                </Widget>
+                <Item>
+                    <span>
+                        <img src="/assets/item-icon.svg" alt="" />
+                        My Items
+                    </span>
+                </Item>
+            </ArtCard>
+            <CommunityCard>
+                <a>
+                    <span>Groups</span>
+                </a>
+                <a>
+                    <span>Events
+                        <img src="/assets/plus-icon.svg" alt="" />
+                    </span>
+                </a>
+                <a>
+                    <span>Follow Hashtags</span>
+                </a>
+                <a>
+                    <span>Discover more</span>
+                </a>
+            </CommunityCard>
         </Container>
     )
 }
@@ -51,6 +83,120 @@ margin: -12px -120px 0;
 
 ;
 `
-const Photo = styled.div``
-const Link = styled.div``
-const AddPhotoText = styled.div``
+const Photo = styled.div`
+box-shadow: none;
+background-image: url("/assets/photo.svg");
+width: 72px;
+height: 72px;
+box-sizing: border-box;
+background-clip: content-box;
+background-position: center;
+background-size: 60%;
+background-repeat: no-repeat;
+border: 2px solid white;
+margin: -38px auto 12px;
+border-radius: 50%;
+`
+const Link = styled.div`
+font-size: 16px;
+line-height: 1.5;
+color: rgba(0, 0, 0, 0.9);
+font-weight: 600;
+`
+const AddPhotoText = styled.div`
+color: #0a66c2;
+margin-top: 4px;
+font-size: 12px;
+line-height: 1.32;
+font-weight: 400;
+`
+
+const Widget = styled.div`
+border-bottom: 1px solid rgba(0, 0, 0, 0.15);
+padding-top: 12px;
+padding-bottom: 12px;
+
+& > a {
+    text-decoration: none;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 4px 12px;
+   
+    &:hover {
+        background-color: rgba(0, 0, 0, 0.08);
+    }
+
+    div {
+        display: flex;
+        text-align: left;
+        flex-direction: column;
+    }
+
+    span {
+        font-size: 12px;
+        line-height: 1.333;
+        &:first-child {
+            color: rgba(0, 0, 0, 0.6);
+        }
+        &:nth-child(2) {
+            color: rgba(0, 0, 0, 1);
+        }
+    }
+
+}
+svg {
+    color: rgba(0, 0, 0, 1);
+}
+`
+
+const Item = styled.a`
+border-color: rgba(0, 0, 0, 0.8);
+text-align: left;
+padding: 12px;
+font-size: 12px;
+display: block;
+span {
+  display: flex;
+  align-items: center;
+  color: rgba(0, 0, 0, 1);
+  svg {
+    color: rgba(0, 0, 0, 0.6);
+  }
+  }
+  
+
+`
+
+const CommunityCard = styled(ArtCard)`
+padding: 8px 0 0;
+text-align: left;
+display: flex;
+flex-direction: column;
+a {
+    color: black;
+        padding: 4px 12px 4px 12px;
+        font-size: 12px;
+
+        &:hover {
+            color: #0a66c2;
+
+        }
+
+        span {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+        }
+        &:last-child{
+            color: rgba(0, 0, 0, 0.6);
+            text-decoration: none;
+            border-top: 1px solid #d6cec2;
+            padding: 12px;
+            &:hover {
+                background-color: rgba(0, 0, 0, 0.08)
+            }
+        }
+    }
+
+`
